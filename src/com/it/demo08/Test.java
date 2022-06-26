40de2bc2ea317079ce55f725324382ff8abbd2ed
itcast2290 <2290303563@qq.com>
2022-06-26 21:11:17 +0800

    测试03

---
+++ src/com/it/demo08/Test.java
@@ -0,0 +1,9 @@
+package com.it.demo08;
+
+public class Test {
+    public static void main(String[] args) {
+       AA08 aa08 = new AA08();
+       // AA08.AC ac = new AA08.AC("ac");
+       // AA08.AB08 ab08 = new AA08().new AB08("ab08");
+    }
+}
