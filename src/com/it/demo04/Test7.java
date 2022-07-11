6eec2735ba3b084ec2809b1f658fa12e633f52bb
itcast2290 <2290303563@qq.com>
2022-07-11 22:20:17 +0800

    提交代码009890

--- src/com/it/demo04/Test7.java
+++ src/com/it/demo04/Test7.java
@@ -4,5 +4,7 @@ public class Test7 {
     public static void main(String[] args) {
         System.out.println("07");
         System.out.println("08");
+
+        System.out.println("98898");
     }
 }
